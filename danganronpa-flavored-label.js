c2c87b1f03c40f9f597f22a989238f5beeaa99ca
yuichiro12 <talcnrhyme@gmail.com>
2017-06-30 01:41:52 +0900

    styleなどいくつかのパラメータをコンストラクタ引数に与えられるよう修正

--- danganronpa-flavored-label.js
+++ danganronpa-flavored-label.js
@@ -1,12 +1,17 @@
 class DanganronpaFlavoredLabel {
 
-	constructor (text, style) {
+	// TODO スクロールのspeedを引数に取れるようにする
+	constructor (text = "Hello! This is Danganronpa Flavored Label.",
+				 style = {},
+				 blink = [100, 40, 100, 30, 100, 30, 20]) {
 		this.text = text;
 		this.label = document.body.appendChild(document.createElement("span"));
 
+		// TODO style["height"]との差分をとる
+		this.maxHeight = style["height"] || "35px";
+		this.label.style.height = style["initHeight"] || "0px";
 		this.label.style.width = style["width"] || "140%";
-		this.label.style.height = style["height"] || "0px";
-		this.label.style.backgroundColor = style["backgroundColor"] || "rgba(208, 11, 111, 1)";
+		this.label.style.backgroundColor = style["backgroundColor"] || "rgba(208, 11, 111, 11)";
 		this.label.style.position = style["position"] || "fixed";
 		this.label.style.top = style["top"] || "50%";
 		this.label.style.bottom = style["bottom"] || "0%";
@@ -15,24 +20,30 @@ class DanganronpaFlavoredLabel {
 		this.label.style.textAlign = style["textAlign"] || "center";
 		this.label.style.color = style["color"] || "#fff";
 		this.label.style.zIndex = style["zIndex"] || "10";
-		this.label.style.lineHeight = style["lineHeight"] || "35px";
+		this.label.style.lineHeight = style["lineHeight"] || style["height"] || "35px";
 
 		this.content = document.createElement("p");
 		this.content.style.display = "none";
 		this.content.innerText = this.text;
 		this.label.appendChild(this.content);
+
+		// TODO 奇数回でない場合にエラーをraise
+		this.blink = blink;
 	}
 
 
-	showLabel (maxHeight) {
+	showLabel () {
 		var height = 0;
+		var H = parseFloat(this.maxHeight.match(/[0-9]+(\.[0-9]+)?/));
+		var unit = String(this.maxHeight.match(/px|pt|em|ex|%/));
 		return new Promise(async (resolve) => {
-			for (var i = 0; i <= maxHeight; i++) {
+			for (var i = 0; i <= H; i++) {
 				await (() => {
 					return new Promise((resolve) => {
 						setTimeout(()=>{
+							// TODO もっと精密にする
 							height++;
-							this.label.style.height = height + "px";
+							this.label.style.height = height + unit;
 							resolve();
 						}, 25);
 					});
@@ -43,17 +54,17 @@ class DanganronpaFlavoredLabel {
 	}
 
 
-	blinkInnerText (delays) {
+	blinkInnerText () {
 		var display = false;
 		return new Promise(async (resolve) => {
-			for (var d of delays) {
+			for (var b of this.blink) {
 				await (() => {
 					return new Promise((resolve) => {
 						setTimeout(()=>{
 							display = display ? false : true;
 							this.content.style.display = display ? "inline" : "none";
 							resolve();
-						}, d);
+						}, b);
 					});
 				})();
 			}
@@ -79,9 +90,8 @@ class DanganronpaFlavoredLabel {
 
 
 	async show () {
-		await this.showLabel(35);
-		// TODO 奇数回でない場合にエラーをraise
-		await this.blinkInnerText([100, 40, 100, 30, 100, 30, 20]);
+		await this.showLabel();
+		await this.blinkInnerText();
 		this.moveInnerText();
 	}
 }
